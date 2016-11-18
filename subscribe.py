ad5137a68850c1314984857d985084c4e741711c
Luís <lzilhao94@gmail.com>
2016-11-18 15:13:46 +0000

    Primeira versao, guarda tempos de chegadas de mensagens

---
+++ subscribe.py
@@ -0,0 +1,41 @@
+import paho.mqtt.client as mqtt
+import time
+import sys
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("topic")
+
+# The callback for when a PUBLISH message is received from the server.
+i=0
+j=0
+f1 = open('subtimes', 'w+')
+
+iterations = int(input("measure time after how many iterations? "))
+def on_message(client, userdata, msg):
+    f1.write(str(client) + " " + str(msg.payload) + str(time.time()) + '\n')
+    global i, j, start_time
+    if i==0:
+        start_time = time.time()
+    i+=1
+    print(str(i)+" "+msg.topic+" "+str(msg.payload))
+    if i==iterations:
+        print(time.time() - start_time)
+        sys.exit()
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
